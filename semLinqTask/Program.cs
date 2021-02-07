8a63d1f9e2f9a916b252df802b34925efcbca583
AndryuschenkoMichael <76478946+AndryuschenkoMichael@users.noreply.github.com>
2021-02-07 11:47:24 +0300

    Add files via upload

---
+++ semLinqTask/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace semLinqTask
+{
+    
+    class Program
+    {
+        private const string PathToFile = @"WeatherEvents_Jan2016-Dec2020.csv";
+        
+        //[SuppressMessage("ReSharper.DPA", "DPA0002: Excessive memory allocations in SOH", MessageId = "type: System.String")]
+        static void Main(string[] args)
+        {
+            //Нужно дополнить модель WeatherEvent, создать список этого типа List<>
+            //И заполнить его, читая файл с данными построчно через StreamReader
+            //Ссылка на файл https://www.kaggle.com/sobhanmoosavi/us-weather-events
+
+            //Написать Linq-запросы, используя синтаксис методов расширений
+            //и продублировать его, используя синтаксис запросов
+            //(возможно с вкраплениями методов расширений, ибо иногда первого может быть недостаточно)
+            
+            //0. Linq - сколько различных городов есть в датасете.
+            //1. Сколько записей за каждый из годов имеется в датасете.
+            //Потом будут еще запросы
+            
+            List<WeatherEvent> weatherEvents = new List<WeatherEvent>();
+            
+            using (StreamReader sr = new StreamReader(PathToFile))
+            {
+                string line = sr.ReadLine();
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    try
+                    {
+                        weatherEvents.Add(new WeatherEvent(line));
+                    }
+                    catch (ArgumentException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    
+                }
+            }
+            
+            
+            // Сколько записей за каждый из годов имеется в датасете. 
+            // weatherEvents
+            //     .GroupBy((x => x.City))
+            //     .Select(x => (x.Key, x.Count()))
+            //     .ToList()
+            //     .ForEach(x => Console.WriteLine($"{x.Key} {x.Item2}"));
+
+            // Количество ивентов в 2018 году.
+            int countAmerica = weatherEvents
+                .Count(x
+                    => x.StartTime.Year == 2018);
+            
+            Console.WriteLine($"-1: Count events on 2018 = {countAmerica}");
+            
+            // Количество различных штатов.
+            int countStates = weatherEvents
+                .GroupBy(x 
+                    => x.State)
+                .Select(x 
+                    => x.Key)
+                .ToList()
+                .Count();
+            
+            // Количество различных городов.
+            int countCity = weatherEvents
+                .GroupBy(x 
+                    => x.City)
+                .Select(x 
+                    => x.Key)
+                .ToList()
+                .Count();
+            
+            Console.WriteLine($"0: Count of States = {countStates}; Count of City = {countCity}");
+            
+            // Топ 3 самых дождливых города в 2019 году. У меня в 2019 - значит начались в 2019.
+            // Если нужно, чтобы они полностью были в 2019, то просто в Where нужно дописать: && x.EndTime.Year = 2018).
+            // P.s. Кортежи в данном случае круче анонимных переменных, т.к. они также могут иметь нормальное имя полей
+            // + к ним подцеплен интерфейс IComparable, значит к ним можно применить функцию Max и она работает корректно
+            Console.WriteLine("1:");
+            
+            var top = weatherEvents
+                .Where(x
+                    => x.StartTime.Year == 2019)
+                .GroupBy(x 
+                    => x.City)
+                .Select(x 
+                    => (City: x.Key, RainDays: x
+                    .Count(x 
+                        => x.WeatherType == WeatherEventType.Rain)))
+                .ToList();
+                
+            top.Sort((x, y) 
+                => y.RainDays - x.RainDays);
+            top
+                .Take(3)
+                .ToList()
+                .ForEach(x 
+                    => Console.WriteLine($"{x.City} rain {x.RainDays}"));
+
+            
+            // Топ снегопадов в Америке по годам.
+            Console.WriteLine("2:");
+
+            weatherEvents
+                .GroupBy(x 
+                    => x.StartTime.Year)
+                .Select(x 
+                    => (Year: x.Key, CityInformation: x
+                    .Where(x 
+                        => x.WeatherType == WeatherEventType.Snow)
+                    .Select(x 
+                        => (Delta: x.EndTime.Ticks - x.StartTime.Ticks, x.StartTime, x.EndTime, x.City))
+                    .Max()))
+                .ToList()
+                .ForEach(x 
+                    => Console.WriteLine($"{x.Year}: | City: {x.Item2.City}; Start time: " +
+                                         $"{x.CityInformation.StartTime}; End time: {x.CityInformation.EndTime}"));
+
+            // string city = weatherEvents
+            //     .GroupBy(x => x.City)
+            //     .Select(x =>
+            //         (x.ToList().Sum(x =>
+            //             (x.WeatherType == WeatherEventType.Rain) ? (x.EndTime - x.StartTime).Ticks : 0), 
+            //         x.Key))
+            //     .Max().Key;
+            // Console.WriteLine(city);
+            
+            
+        }
+    }
+}
---
+++ semLinqTask/WeatherEvent.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace semLinqTask
+{
+    class WeatherEvent
+    {
+        public string EventId { get; set; }
+        public WeatherEventType WeatherType { get; set; }
+        public Severitys Severity { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string TimeZone { get; set; }
+        public string AirportZone { get; set; }
+        public double LocationLat { get; set; }
+        public double LocationLng { get; set; }
+        public string City { get; set; }
+        
+        public string Country { get; set; }
+        
+        public string State { get; set; }
+        
+        public string ZipCode { get; set; }
+
+        public WeatherEvent(string input)
+        {
+            try
+            {
+                string[] inputLines = input.Split(',');
+                EventId = inputLines[0];
+                
+                if (!Enum.TryParse(inputLines[1], out WeatherEventType weather))
+                {
+                    weather = WeatherEventType.Unknown;
+                }
+
+                WeatherType = weather;
+
+
+                if (!Severitys.TryParse(inputLines[2], out Severitys severity))
+                {
+                    severity = Severitys.Unknown;
+                }
+
+                Severity = severity;
+
+                StartTime = DateTime.Parse(inputLines[3]);
+                EndTime = DateTime.Parse(inputLines[4]);
+                TimeZone = inputLines[5];
+                AirportZone = inputLines[6];
+                LocationLat = Double.Parse(inputLines[7].Replace('.', ','));
+                LocationLng = Double.Parse(inputLines[8].Replace('.', ','));
+                City = inputLines[9];
+                Country = inputLines[10];
+                State = inputLines[11];
+                ZipCode = inputLines[12];
+            }
+            catch
+            {
+                throw new ArgumentException($"Can't parse string: '{input}', to WeatherEvent");
+            }
+        }
+
+    }
+
+    //Дополнить перечисления
+    enum WeatherEventType
+    {
+        Unknown,
+        Snow,
+        Fog,
+        Cold,
+        Storm,
+        Rain,
+        Precipitation,
+        Hail,
+    }
+
+    enum Severitys
+    {
+        Unknown,
+        Light,
+        Severe,
+        Moderate,
+        Heavy,
+        UNK,
+        Other,
+    }
+}
