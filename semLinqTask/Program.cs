91953ab14cbcba5b827384375c441cd84108fe00
Misha <maandryuschenko@edu.hse.ru>
2021-02-08 21:02:41 +0300

    Решение заданий методом запросов

--- semLinqTask/Program.cs
+++ semLinqTask/Program.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Authentication;
 
 namespace semLinqTask
 {
     
     class Program
     {
-        private const string PathToFile = @"WeatherEvents_Jan2016-Dec2020.csv";
+        private const string PathToFile = @"D:\Projects C#\WeatherEvents_Jan2016-Dec2020.csv";
         
         //[SuppressMessage("ReSharper.DPA", "DPA0002: Excessive memory allocations in SOH", MessageId = "type: System.String")]
         static void Main(string[] args)
@@ -47,93 +49,81 @@ namespace semLinqTask
             
             
             // Сколько записей за каждый из годов имеется в датасете. 
-            // weatherEvents
-            //     .GroupBy((x => x.City))
-            //     .Select(x => (x.Key, x.Count()))
-            //     .ToList()
-            //     .ForEach(x => Console.WriteLine($"{x.Key} {x.Item2}"));
+
+            // var countEventsOfCitys =
+            //     from weatherEvent in weatherEvents
+            //     group weatherEvent by weatherEvent.City
+            //     into cityGroup
+            //     select (City: cityGroup.Key, Count: cityGroup.Count());
+            //
+            // foreach (var (city, count) in countEventsOfCitys)
+            // {
+            //     Console.WriteLine($"{city} {count}");
+            // }
 
             // Количество ивентов в 2018 году.
-            int countAmerica = weatherEvents
-                .Count(x
-                    => x.StartTime.Year == 2018);
+            int countAmerica = weatherEvents.Count(x => x.StartTime.Year == 2018);
             
             Console.WriteLine($"-1: Count events on 2018 = {countAmerica}");
-            
-            // Количество различных штатов.
-            int countStates = weatherEvents
-                .GroupBy(x 
-                    => x.State)
-                .Select(x 
-                    => x.Key)
-                .ToList()
-                .Count();
-            
+
+            int countStates = 
+                (from weatherEvent in weatherEvents 
+                    group weatherEvent by weatherEvent.State
+                    into statesGroup
+                    select statesGroup.Key).Count();
+
             // Количество различных городов.
-            int countCity = weatherEvents
-                .GroupBy(x 
-                    => x.City)
-                .Select(x 
-                    => x.Key)
-                .ToList()
-                .Count();
-            
+            int countCity =
+                (from weatherEvent in weatherEvents
+                    group weatherEvent by weatherEvent.City
+                    into cityGroup
+                    select cityGroup.Key).Count();
+
             Console.WriteLine($"0: Count of States = {countStates}; Count of City = {countCity}");
             
             // Топ 3 самых дождливых города в 2019 году. У меня в 2019 - значит начались в 2019.
-            // Если нужно, чтобы они полностью были в 2019, то просто в Where нужно дописать: && x.EndTime.Year = 2018).
+            // Если нужно, чтобы они полностью были в 2019, то просто в Where нужно дописать: && x.EndTime.Year = 2019).
             // P.s. Кортежи в данном случае круче анонимных переменных, т.к. они также могут иметь нормальное имя полей
             // + к ним подцеплен интерфейс IComparable, значит к ним можно применить функцию Max и она работает корректно
             Console.WriteLine("1:");
-            
-            var top = weatherEvents
-                .Where(x
-                    => x.StartTime.Year == 2019)
-                .GroupBy(x 
-                    => x.City)
-                .Select(x 
-                    => (City: x.Key, RainDays: x
-                    .Count(x 
-                        => x.WeatherType == WeatherEventType.Rain)))
-                .ToList();
-                
-            top.Sort((x, y) 
-                => y.RainDays - x.RainDays);
-            top
-                .Take(3)
-                .ToList()
-                .ForEach(x 
-                    => Console.WriteLine($"{x.City} rain {x.RainDays}"));
 
+            var top =
+                from weatherEvent in weatherEvents
+                where weatherEvent.StartTime.Year == 2019
+                group weatherEvent by weatherEvent.City
+                into cityGroup
+                let rainDays = cityGroup.Count(x => x.WeatherType == WeatherEventType.Rain)
+                orderby rainDays descending 
+                select (City: cityGroup.Key,
+                    RainDays: rainDays);
+
+
+            foreach (var (city, rainDays) in top.Take(3))
+            {
+                Console.WriteLine($"{city} rain {rainDays}");
+            }
             
             // Топ снегопадов в Америке по годам.
             Console.WriteLine("2:");
 
-            weatherEvents
-                .GroupBy(x 
-                    => x.StartTime.Year)
-                .Select(x 
-                    => (Year: x.Key, CityInformation: x
-                    .Where(x 
-                        => x.WeatherType == WeatherEventType.Snow)
-                    .Select(x 
-                        => (Delta: x.EndTime.Ticks - x.StartTime.Ticks, x.StartTime, x.EndTime, x.City))
-                    .Max()))
-                .ToList()
-                .ForEach(x 
-                    => Console.WriteLine($"{x.Year}: | City: {x.Item2.City}; Start time: " +
-                                         $"{x.CityInformation.StartTime}; End time: {x.CityInformation.EndTime}"));
+            var topYears =
+                from weatherEvent in weatherEvents
+                group weatherEvent by weatherEvent.StartTime.Year
+                into yearGroup
+                select (Year: yearGroup.Key,
+                    CityInformation:
+                    (from yearElement in yearGroup
+                        where yearElement.WeatherType == WeatherEventType.Snow
+                        select (Delta: yearElement.EndTime.Ticks - yearElement.StartTime.Ticks,
+                            StartTime: yearElement.StartTime,
+                            EndTime: yearElement.EndTime,
+                            City: yearElement.City)).Max());
 
-            // string city = weatherEvents
-            //     .GroupBy(x => x.City)
-            //     .Select(x =>
-            //         (x.ToList().Sum(x =>
-            //             (x.WeatherType == WeatherEventType.Rain) ? (x.EndTime - x.StartTime).Ticks : 0), 
-            //         x.Key))
-            //     .Max().Key;
-            // Console.WriteLine(city);
-            
-            
+            foreach (var (year, cityInformation) in topYears)
+            {
+                Console.WriteLine($"{year}: | City: {cityInformation.City}; Start time: " +
+                                  $"{cityInformation.StartTime}; End time: {cityInformation.EndTime}");
+            }
         }
     }
 }
